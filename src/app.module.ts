2427fd4f997b5890ab235d7292565ae1655d0d4c
gmartins9042 <gmartins9042@gmail.com>
2025-08-11 18:10:58 -0300

    Deploy do Projeto Blog Pessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,41 +1,28 @@
-// Importa o decorator Module do NestJS para definir módulos
 import { Module } from '@nestjs/common';
-// Importa o módulo TypeOrmModule para integrar TypeORM com NestJS
 import { TypeOrmModule } from '@nestjs/typeorm';
-// Importa a entidade Postagem para registrar no TypeORM
-import { Postagem } from './postagem/entities/postagem.entity';
-// Importa o módulo que cuida das funcionalidades relacionadas à Postagem
 import { PostagemModule } from './postagem/postagem.module';
-import { Tema } from './tema/entities/tema.entity';
 import { TemaModule } from './tema/tema.module';
 import { AuthModule } from './auth/auth.module';
 import { UsuarioModule } from './usuario/usuario.module';
-import { Usuario } from './usuario/entities/usuario.entity';
 import { AppController } from './app.controller';
+import { ConfigModule } from '@nestjs/config';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
-  // Configurações dos módulos que serão carregados na aplicação
   imports: [
-    // Configura o TypeORM para conectar com o banco MySQL
-    TypeOrmModule.forRoot({
-      type: 'mysql',          // Tipo do banco de dados
-      host: 'localhost',      // Endereço do servidor do banco
-      port: 3306,             // Porta do banco MySQL padrão
-      username: 'root',       // Usuário do banco
-      password: '8486',       // Senha do banco (cuidado com exposição!)
-      database: 'db_blogpessoal',  // Nome do banco de dados a ser usado
-      entities: [Postagem, Tema, Usuario, ],   // Entidades que o TypeORM deve gerenciar
-      synchronize: true,      // Sincroniza o esquema do banco automaticamente (bom para dev)
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      useClass: ProdService,
+      imports: [ConfigModule],
     }),
-    // Importa o módulo Postagem para registrar suas rotas e serviços
-    PostagemModule, 
-    TemaModule, // Importa o módulo Tema para registrar suas rotas e serviços
-    AuthModule, // Importa o módulo Auth para gerenciar autenticação e segurança
-    UsuarioModule, // Importa o módulo Usuario para gerenciar usuários
+    PostagemModule,
+  TemaModule, // Importa o módulo Tema para registrar suas rotas e serviços
+  AuthModule, // Importa o módulo Auth para gerenciar autenticação e segurança
+  UsuarioModule, // Importa o módulo Usuario para gerenciar usuários
   ],
   // Controllers globais (nenhum declarado aqui)
   controllers: [AppController],
   // Providers globais (nenhum declarado aqui)
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
